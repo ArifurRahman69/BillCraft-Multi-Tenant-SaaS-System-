c09f2f67c3d8d9c1719256858705d843c54b7301
ArifurRahman69 <arifurr953@gmail.com>
2026-06-22 19:55:31 +0600

    1st push

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BillCraft__Multi_Tenant_SaaS_System_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
